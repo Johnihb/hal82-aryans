0f1f8518a83933e277300f7982b12ef85a822340
Amrit_Xtha <keshwor12@gmail.com>
2026-02-20 18:26:30 +0545

    added a setting page with backend logic

---
+++ app/settings/page.tsx
@@ -0,0 +1,38 @@
+
+
+import Settings from '@/components/settings/Settings'
+import { auth } from '@/lib/auth'
+import { userStatus } from '@/lib/user'
+import { headers } from 'next/headers'
+
+export interface User {
+  phoneNumber: string
+  username: string
+  email: string
+  image: string
+  createdAt: Date
+  [key:string]: any  
+}
+
+const page = async () => {
+
+  const session = await auth.api.getSession({headers:await headers()})
+ 
+  
+  const user = await userStatus();
+
+  const userData:User = {
+    ...user,
+    phoneNumber: user?.phoneNumber || '',
+    username: session?.user?.name || '',
+    email: session?.user?.email || '',
+    image: session?.user?.image || '/default/defaultUser.jfif',
+    createdAt: session?.user?.createdAt || new Date(),
+  }
+
+  return (
+    <Settings userData={userData}   />
+  )
+}
+
+export default page
